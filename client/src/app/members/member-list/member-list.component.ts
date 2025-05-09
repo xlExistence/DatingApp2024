0118d67a68a9a1de274c51a2af34acb41b40145a
Josué Polina <josue.polina19122002@gmail.com>
2025-05-09 10:18:27 -0600

    Parcial05 - Add filter buttons to SPA

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -4,11 +4,12 @@ import { MemberCardComponent } from '../member-card/member-card.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { AccountService } from '../../_services/account.service';
 import { UserParams } from '../../_models/userParams';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent, PaginationModule],
+  imports: [MemberCardComponent, PaginationModule, FormsModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
@@ -16,12 +17,19 @@ export class MemberListComponent implements OnInit {
   private accountService = inject(AccountService);
   membersService = inject(MembersService);
   userParams = new UserParams(this.accountService.currentUser());
+  genderList = [{value: "female", display: "Females"}, {value: "male", display: "Males"}]
 
   ngOnInit(): void {
     if (!this.membersService.paginatedResult()) {
       this.loadMembers();
     }
   }
+
+  resetFilters() {
+    this.userParams = new UserParams(this.accountService.currentUser());
+    this.loadMembers();
+  }
+
 
   loadMembers() {
     this.membersService.getMembers(this.userParams);
