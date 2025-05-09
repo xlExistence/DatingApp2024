b05ce3ef67cf0a0ec68d843828889ccb0de8843c
Josué Polina <102565353+xlExistence@users.noreply.github.com>
2025-05-09 09:38:24 -0600

    Parcial04 (#1)
    * Parcial04 - Add PhotoService
    
    * Parcial04 - Add SPA Photo Editor component
    
    * Parcial04 - Add ng2-file-upload
    
    * Parcial04 - Add photo uploader
    
    * Parcial04 - Add PUT for photos
    
    * Parcial014 - Add photo to nav bar
    
    * Parcial04 - Set main photo in client
    
    * Parcial04 - Delete photo from API
    
    * Parcial04 - Delete photo from client
    
    * Parcial04 - Initialize Reactive form
    
    * Parcial04 - Add client validation
    
    * Parcial04 - Add custom validator
    
    * Parcial04 - Add feedback to FormGroup
    
    * Parcial04 - Add a reusable text input component
    
    * Parcial04 - Add FormBuilder
    
    * Parcial04 - Fix text-input load error
    
    * Parcial04 - Add more fields to registration form
    
    * Parcial04 - Add a reusable date picker component
    
    * Parcial04 - Update API user registration
    
    * Parcial04 - Update client side user registration

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { of, tap } from 'rxjs';
+import { Photo } from '../_models/photo';
 
 @Injectable({
   providedIn: 'root'
@@ -29,8 +30,34 @@ export class MembersService {
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + "users", member).pipe(
       tap(() => {
-        this.members.update(members => 
+        this.members.update(members =>
           members.map(m => m.userName === member.userName ? member : m))
+      })
+    );
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + "users/photo/" + photo.id, {}).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => {
+          if (m.photos.includes(photo)) {
+            m.photoUrl = photo.url;
+          }
+          return m;
+        }))
+      })
+    );
+  }
+
+  deletePhoto(photo: Photo) {
+    return this.http.delete(this.baseUrl + "users/photo/" + photo.id).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => {
+          if (m.photos.includes(photo)) {
+            m.photos = m.photos.filter(p => p.id !== photo.id)
+          }
+          return m;
+        }))
       })
     );
   }
