e950ecc352c7ef96390b74fd503a500d8ebe71b1
Josué Polina <josue.polina19122002@gmail.com>
2025-06-25 22:31:34 -0600

    Parcial06 - Add send message to Message Hub

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -1,10 +1,14 @@
 namespace API.SignalR;
 
+using System.Globalization;
 using API.Data;
+using API.DataEntities;
+using API.DTOs;
 using API.Extensions;
+using AutoMapper;
 using Microsoft.AspNetCore.SignalR;
 
-public class MessageHub(IMessageRepository messagesRepository) : Hub
+public class MessageHub(IMessageRepository messagesRepository, IUserRepository userRepository, IMapper mapper) : Hub
 {
     public override async Task OnConnectedAsync()
     {
@@ -13,7 +17,7 @@ public class MessageHub(IMessageRepository messagesRepository) : Hub
 
         if (Context.User == null || string.IsNullOrEmpty(otherUser))
         {
-            throw new Exception("Cannot join group");
+            throw new HubException("Cannot join group");
         }
         var groupName = GetGroupName(Context.User.GetUserName(), otherUser);
         await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
@@ -22,6 +26,41 @@ public class MessageHub(IMessageRepository messagesRepository) : Hub
     }
 
     public override Task OnDisconnectedAsync(Exception? exception) => base.OnDisconnectedAsync(exception);
+
+    public async Task SendMessage(MessageRequest messageRequest)
+    {
+        var username = Context.User?.GetUserName() ?? throw new ArgumentException("Could not get user");
+
+        if (username == messageRequest.RecipientUsername.ToLower(CultureInfo.InvariantCulture))
+        {
+            throw new HubException("You cannnot message yourself");
+        }
+
+        var sender = await userRepository.GetByUsernameAsync(username);
+        var recipient = await userRepository.GetByUsernameAsync(messageRequest.RecipientUsername);
+
+        if (recipient == null || sender == null || sender.UserName == null || recipient.UserName == null)
+        {
+            throw new HubException("The message can't be sent right now");
+        }
+
+        var message = new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = sender.UserName,
+            RecipientUsername = recipient.UserName,
+            Content = messageRequest.Content
+        };
+
+        messagesRepository.Add(message);
+
+        if (await messagesRepository.SaveAllAsync())
+        {
+            var group = GetGroupName(sender.UserName, recipient.UserName);
+            await Clients.Group(group).SendAsync("NewMessage", mapper.Map<MessageResponse>(message));
+        }
+    }
 
     private string GetGroupName(string caller, string? other)
     {
