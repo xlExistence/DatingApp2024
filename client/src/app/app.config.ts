e9f1160d9e5e7dd7ed36f6fb618660f1078efc94
Josué Polina <josue.polina19122002@gmail.com>
2025-06-25 21:56:12 -0600

    Parcial06 - Add modal configuration

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -11,6 +11,7 @@ import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { loadingInterceptor } from './_interceptors/loading.interceptor';
 import { TimeagoModule } from 'ngx-timeago';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,7 +21,7 @@ export const appConfig: ApplicationConfig = {
     provideToastr({
       positionClass: "toast-bottom-right"
     }),
-    importProvidersFrom(NgxSpinnerModule, TimeagoModule.forRoot()),
+    importProvidersFrom(NgxSpinnerModule, TimeagoModule.forRoot(), ModalModule.forRoot()),
     {
       provide: GALLERY_CONFIG,
       useValue: {
