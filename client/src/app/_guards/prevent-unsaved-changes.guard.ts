4da68119ec6bd34e632a7336792f24cb6a2ddc55
Josué Polina <josue.polina19122002@gmail.com>
2025-06-26 00:04:48 -0600

    Parcial06 - Add Confirm Service to SPA

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,9 +1,14 @@
 import { CanDeactivateFn } from '@angular/router';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
+import { inject } from '@angular/core';
 
 export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
+  const confirmService = inject(ConfirmService);
+
   if (component.editForm?.dirty) {
-    return confirm("If you don't save, the changes will be lost, continue?");
+    return confirmService.confirm() ?? false;
   }
+
   return true;
 };
---
+++ client/src/app/_services/confirm.service.ts
@@ -0,0 +1,38 @@
+import { inject, Injectable } from '@angular/core';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { map } from 'rxjs';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmService {
+  bsModalRef?: BsModalRef;
+  private modalService = inject(BsModalService);
+
+  confirm(
+    title = "Confirmation",
+    message = "Are you sure you want to leave?",
+    btnOkText = "Ok",
+    btnCancelText = "Cancel"
+  ) {
+    const config: ModalOptions = {
+      initialState: {
+        title,
+        message,
+        btnOkText,
+        btnCancelText
+      }
+    };
+    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config);
+    return this.bsModalRef.onHidden?.pipe(
+      map(() => {
+        if (this.bsModalRef?.content) {
+          return this.bsModalRef.content.result;
+        } else {
+          return false;
+        }
+      })
+    )
+  }
+}
