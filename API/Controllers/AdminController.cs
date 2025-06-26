b1b9213376b19db59950754e4d1414913bb30c1f
Josué Polina <josue.polina19122002@gmail.com>
2025-06-25 21:28:38 -0600

    Parcial06 - Edit roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,15 +1,56 @@
 namespace API.Controllers;
 
+using API.DataEntities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class AdminController : BaseApiController
+public class AdminController(UserManager<AppUser> userManager) : BaseApiController
 {
     [Authorize(Policy = "RequireAdminRole")]
     [HttpGet("users-with-roles")]
-    public ActionResult GetUsersWithRole()
+    public async Task<ActionResult> GetUsersWithRole()
     {
-        return Ok("Admin access granted!");
+        var users = await userManager.Users
+            .OrderBy(x => x.UserName)
+            .Select(x => new
+            {
+                x.Id,
+                Username = x.UserName,
+                Roles = x.UserRoles.Select(r => r.Role.Name).ToList()
+            }).ToListAsync();
+
+        return Ok(users);
+    }
+
+    [Authorize(Policy = "RequireAdminRole")]
+    [HttpPost("{username}")]
+    public async Task<ActionResult> EditRoles(string username, string roles)
+    {
+        if (string.IsNullOrEmpty(roles))
+        {
+            return BadRequest("You must provide at least one role");
+        }
+
+        var selectedRoles = roles.Split(",").ToArray();
+        var user = await userManager.FindByNameAsync(username);
+
+        if (user == null)
+        {
+            return BadRequest("User doesn't exist");
+        }
+
+        var userRoles = await userManager.GetRolesAsync(user);
+        var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to add to roles");
+
+        result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to remove from roles");
+
+        return Ok(await userManager.GetRolesAsync(user));
     }
 
     [Authorize(Policy = "ModeratePhotoRole")]
