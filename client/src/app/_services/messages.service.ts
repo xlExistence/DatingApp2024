99e748df0c3389ea127aa1497fe5f25e30ce9bdc
Josué Polina <josue.polina19122002@gmail.com>
2025-06-25 22:41:37 -0600

    Parcial06 - Add sending messages using SugnalR Hub

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -31,6 +31,10 @@ export class MessagesService {
     this.hubConnection.on("ReceiveMessageThread", messages => {
       this.messageThread.set(messages);
     });
+
+    this.hubConnection.on("NewMessage", message => {
+      this.messageThread.update(messages => [...messages, message]);
+    })
   }
 
   stopHbuConnection() {
@@ -53,8 +57,8 @@ export class MessagesService {
     return this.http.get<Message[]>(this.baseUrl + "messages/thread/" + username);
   }
 
-  sendMessage(username: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + "messages", { recipientUsername: username, content });
+  async sendMessage(username: string, content: string) {
+    return this.hubConnection?.invoke("SendMessage", { recipientUsername: username, content});
   }
 
   deleteMessage(id: number) {
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -17,10 +17,8 @@ export class MemberMessagesComponent {
   messageContent = "";
 
   sendMessage() {
-    this.messagesService.sendMessage(this.username(), this.messageContent).subscribe({
-      next: message => {
-        this.messageForm?.reset();
-      }
-    });
+    this.messagesService.sendMessage(this.username(), this.messageContent).then(() => {
+      this.messageForm?.reset();
+    })
   }
 }
